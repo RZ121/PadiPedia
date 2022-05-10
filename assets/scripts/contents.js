917c28efccfd56a1cfee8202b5c5e80c8dd0a2a9
M. Rizky Azizi <ajijiren.alpha10@gmail.com>
2022-05-10 22:53:55 +0700

    menyelesaikan fungsi diagnosa hama yang tidak terdaftar pada rule

--- assets/scripts/contents.js
+++ assets/scripts/contents.js
@@ -33,14 +33,12 @@ if(cardList){
             dataCardImage.src = `../assets/img/hama/${type['gambar']}`;
             dataCardInfo.setAttribute("class","data-card-info");
             
-            let i = 1;
             data["gejala"][type["gejala"]].forEach(el => {
                 const dataCardList = document.createElement("li");
                 dataCardList.setAttribute("class","rf-15");
                 const textContent = document.createTextNode(el);
                 dataCardList.appendChild(textContent);
                 dataCardUl.appendChild(dataCardList);
-                i++;
             });
             dataCardInfo.innerHTML = `<p class="rf-15"> Gejala yang ditimbulkan : </p>`;
             dataCardInfo.appendChild(dataCardUl);
@@ -52,7 +50,24 @@ if(cardList){
     });
 }
 
-//Menampilkan alert hasil diagnosa (sementara)
+//Menampilkan alert hasil diagnosa
+const fc = document.querySelector(".floating-card");
+const fcImg = document.querySelector(".floating-card-image");
+const fcTitle = document.querySelector(".floating-card-title");
+const fcSubtitle = document.querySelector(".floating-card-subTitle");
+const fcText = document.querySelector(".floating-card-text");
+const fcSection = document.querySelector(".floating-card-section");
+const closeBtn = document.querySelector(".close-button");
+
+closeBtn.addEventListener("click",() => {
+    fc.classList.remove("active");
+    fcImg.src = "";
+    fcTitle.innerText = "";
+    fcSubtitle.innerHTML = "";
+    fcText.innerText = "";
+    fcSection.innerHTML = "";
+});
+
 function showDiagnoseResult(kodeHama,nilaiKepastian){
     fetch("../assets/data/database.json")
     .then(response => {
@@ -67,6 +82,36 @@ function showDiagnoseResult(kodeHama,nilaiKepastian){
             idx++;
         }
         const hama = data["hama"][idx];
-        alert(`Diagnosa Hama :\nNama : ${hama["nama"]}\nLatin : ${hama["latin"]}\nNilai Kemungkinan : ${nilaiKepastian}%`);
+        fc.classList.add("active");
+        fcImg.src = `../assets/img/hama/${hama["gambar"]}`;
+        fcTitle.innerText = hama["nama"];
+        fcSubtitle.innerHTML = `<i>${hama["latin"]}</i>`;
+        fcText.innerText = `(${nilaiKepastian}% Akurat)`;
+
+        const dataCardUl = document.createElement("ol");
+        data["gejala"][hama["gejala"]].forEach(el => {
+            const dataCardList = document.createElement("li");
+            dataCardList.setAttribute("class","rf-15");
+            const textContent = document.createTextNode(el);
+            dataCardList.appendChild(textContent);
+            dataCardUl.appendChild(dataCardList);
+        });
+        fcSection.innerHTML = `<p class="rf-15"> Gejala yang ditimbulkan : </p>`;
+        fcSection.appendChild(dataCardUl);
     });
 }
+
+//mencegah user menginput angka diluar range
+const number_inputs = document.querySelectorAll('.input-cf');
+number_inputs.forEach(el => {
+    el.onkeypress = function (e) {
+        var ev = e || window.event;
+        if(ev.charCode < 48 || ev.charCode > 57) {
+            return false; // not a digit
+        } else if(this.value * 10 + ev.charCode - 48 > this.max) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+});
--- assets/scripts/process.js
+++ assets/scripts/process.js
@@ -1,13 +1,14 @@
 const userInput = {};
 const userInputCf = {};
 
-const diagnose_btn = document.querySelector(".btn-form");
+const diagnose_btn = document.querySelector(".run-form");
 diagnose_btn.addEventListener("click", () => {
     const input1 = document.getElementsByName("gejala_akar");
     for(ga of input1){
         if(ga.checked){
             userInput["GA"] = ga.value;
             const GAcf = document.getElementById("GA-cf");
+            if(GAcf.value === "") GAcf.value = 100;
             userInputCf["GA"] = GAcf.value;
         }
     }
@@ -16,6 +17,7 @@ diagnose_btn.addEventListener("click", () => {
         if(gb.checked){
             userInput["GB"] = gb.value;
             const GBcf = document.getElementById("GB-cf");
+            if(GBcf.value === "") GAcf.value = 100;
             userInputCf["GB"] = GBcf.value;
         }
     }
@@ -24,6 +26,7 @@ diagnose_btn.addEventListener("click", () => {
         if(gd.checked){
             userInput["GD"] = gd.value;
             const GDcf = document.getElementById("GD-cf");
+            if(GDcf.value === "") GAcf.value = 100;
             userInputCf["GD"] = GDcf.value;
         }
     }
@@ -32,6 +35,7 @@ diagnose_btn.addEventListener("click", () => {
         if(gu.checked){
             userInput["GU"] = gu.value;
             const GUcf = document.getElementById("GU-cf");
+            if(GUcf.value === "") GAcf.value = 100;
             userInputCf["GU"] = GUcf.value;
         }
     }
@@ -44,8 +48,9 @@ function getHama(){
         return response.json();
     })
     .then(data => {
-        let working_memory = {};
-        let working_memoryCf = {};
+        const working_memory = {};
+        const working_memoryCf = {};
+
         data["set1"].forEach(rule => {
             const keys = Object.keys(rule["gejala"]);
             let count = 0;
@@ -81,10 +86,56 @@ function getHama(){
             }
         }
         const certaintyValue = (minimumCf * working_memory["CFrule"]).toFixed(2);
-        showDiagnoseResult(working_memory["hama"],certaintyValue);
+        
+        if(!working_memory["hama"]){
+            getHamaUnlistedRule();
+        }else{
+            showDiagnoseResult(working_memory["hama"], certaintyValue);
+        }
     });
 }
 
 function getHamaUnlistedRule(){
-  //Fungsi direncanakan untuk eksekusi proses diagnosa yang tidak terdaftar didalam rule
+
+  fetch("../assets/data/ruleSet.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const working_memory = {};
+        const working_memoryCf = {};
+
+        data["set1"].forEach(rule => {
+            const keys = Object.keys(rule["gejala"]);
+            let count = 0;
+            for(kategori of keys){
+                if(!working_memory[kategori]){
+                    if(userInput[kategori]){
+                        working_memory[kategori] = userInput[kategori];
+                    }
+                }
+                if(working_memory[kategori] === rule["gejala"][kategori]){
+                    count++;
+                }
+            }
+            working_memoryCf[rule["hama"]] = (((count/keys.length)*100 + (rule["cf"] * 100))/2).toFixed(2);
+        });
+        const result = maxValue(working_memoryCf);
+        showDiagnoseResult(result["kode hama"], result["cf"]);
+    })
+}
+
+function maxValue(obj){
+    const keys = Object.keys(obj);
+    let index = 0;
+    for(i = 1; i < keys.length; i++){
+        if(obj[keys[i]] > obj[keys[index]]){
+            index = i;
+        }
+    }
+    const objResult = {
+        "kode hama" : keys[index],
+        "cf" : obj[keys[index]]
+    }
+    return objResult;
 }
